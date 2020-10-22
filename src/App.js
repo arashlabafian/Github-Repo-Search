3271f0fe279a787b182963fed8585c0808f4e853
arash_labafian <arashlabafian@gmail.com>
2020-10-22 15:48:51 +0330

    initial page routes added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const loading = <div>loading</div>;
+
+const Layout = React.lazy(() => import('./containers/Layout'));
+const Login = React.lazy(() => import('./views/pages/Login'));
+const Page404 = React.lazy(() => import('./views/pages/Page404'));
+const Page500 = React.lazy(() => import('./views/pages/Page500'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <React.Suspense fallback={loading}>
+        <Switch>
+          <Route
+            exact
+            path='/login'
+            name='Login Page'
+            render={props => <Login {...props} />}
+          />
+          <Route
+            exact
+            path='/404'
+            name='Page 404'
+            render={props => <Page404 {...props} />}
+          />
+          <Route
+            exact
+            path='/500'
+            name='Page 500'
+            render={props => <Page500 {...props} />}
+          />
+          <Route path='/' name='Home' render={props => <Layout {...props} />} />
+        </Switch>
+      </React.Suspense>
+    </BrowserRouter>
   );
 }
 
